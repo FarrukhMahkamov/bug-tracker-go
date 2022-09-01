b97463dbd736975151b3a7894cdf975955a6cade
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2022-09-01 22:26:23 +0500

    connection to database succed. Change config env names

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/FarrukhMahkamov/bugtracker/internal/handler"
 	"github.com/FarrukhMahkamov/bugtracker/internal/repository"
 	"github.com/FarrukhMahkamov/bugtracker/internal/service"
+	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -17,7 +18,20 @@ func main() {
 	}
 	logrus.SetFormatter(new(logrus.JSONFormatter))
 
-	repositories := repository.NewRepository()
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBname:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatalf("Error while initializing database: %s", err.Error())
+	}
+
+	repositories := repository.NewRepository(db)
 	services := service.NewService(repositories)
 	handlers := handler.NewHandler(services)
 	srv := new(server.Server)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.2.0
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/viper v1.12.0
 )
