8816f08163523d6575209e0d8d20f9bafb8468bd
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2022-10-05 17:43:39 +0500

    Fix the eroro in sql file

--- BUG TRACKER.session.sql
+++ BUG TRACKER.session.sql
@@ -1,1 +1,1 @@
-UPDATE schema_migrations SET version='000001', dirty=FALSE
+UPDATE schema_migrations SET version='000003', dirty=FALSE
--- migrations/000003_database.down.sql
+++ migrations/000003_database.down.sql
@@ -1,5 +1,6 @@
 DROP TABLE bugs_tags;
 
+
 DROP TABLE projects_users;
 
 DROP TABLE bugs_users;
@@ -10,6 +11,8 @@ DROP TABLE users;
 
 DROP TABLE job_types;
 
+DROP TABLE bugs;
+
 DROP TABLE teams;
 
 DROP TABLE tags;
@@ -18,7 +21,6 @@ DROP TABLE categories;
 
 DROP TABLE statuses;
 
-DROP TABLE bugs;
 
 
 
