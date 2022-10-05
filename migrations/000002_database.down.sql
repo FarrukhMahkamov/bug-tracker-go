f84fce277ef12e9502c13de13723ee970319edf3
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2022-10-05 17:36:36 +0500

    Add new tables

--- migrations/000002_database.down.sql
+++ migrations/000002_database.down.sql
@@ -1,1 +1,25 @@
-UPDATE schema_migrations SET version='2', dirty=FALSE
+DROP TABLE bugs_tags;
+
+DROP TABLE projects_users;
+
+DROP TABLE bugs_users;
+
+DROP TABLE teams_users;
+
+DROP TABLE users;
+
+DROP TABLE job_types;
+
+DROP TABLE teams;
+
+DROP TABLE tags;
+
+DROP TABLE categories;
+
+DROP TABLE statuses;
+
+DROP TABLE bugs;
+
+
+
+
